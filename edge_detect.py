cf73031778b989d8f0fc35a489470a1987d251bd
Candy Dong <qingyid@andrew.cmu.edu>
2019-03-09 18:05:31 -0500

    added final result

--- edge_detect.py
+++ edge_detect.py
@@ -1,5 +1,25 @@
-import cv2
+import cv2, sys
+import numpy as np
 
-img = cv2.imread(sys.argv[1])
-edges = cv2.Canny(img, 100, 200)
-cv2.imwrite(sys.argv[2], edges)
+im_gray = cv2.imread(sys.argv[1], cv2.IMREAD_GRAYSCALE)
+
+(thresh, im_bw) = cv2.threshold(im_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+# cv2.imwrite("grayscale.png", im_bw)
+
+print(sys.argv[1])
+
+
+# edges = cv2.Canny(im_bw, 100, 200)
+# cv2.imwrite(sys.argv[2], edges)
+im_blur = cv2.GaussianBlur(im_bw,(15,15),0)
+
+for i in range(10):
+	im = cv2.filter2D(im_blur, -1, np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]]))
+	# im_blur = cv2.blur(im_bw,(10,10))  
+
+	(thresh_2, im_bw) = cv2.threshold(im, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+
+
+cv2.imwrite("result.png", im_bw)
